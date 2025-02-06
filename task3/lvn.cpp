5b775a6603c273db9e008057319a8e4331a0c198
Lisa Li <lisaruiwen04@gmail.com>
2025-02-06 18:00:54 -0500

    Implement LVN table properly

--- task3/lvn.cpp
+++ task3/lvn.cpp
@@ -4,11 +4,12 @@
 #include <set>
 #include <unordered_map>
 #include <format>
+#include <cassert>
 #include <nlohmann/json.hpp>
 
 #include "../task2/cfg/cfg_utils.hpp"
 
-const std::set<std::string> known_ops = {"add", "sub", "mul", "div", "eq", "lt", "gt", "le", "ge", "and", "or", "not", "fadd", "fsub", "fmul", "fdiv", "feq", "flt", "fgt", "fle", "fge", "const"};
+const std::set<std::string> known_ops = {"add", "sub", "mul", "div", "eq", "lt", "gt", "le", "ge", "and", "or", "not", "fadd", "fsub", "fmul", "fdiv", "feq", "flt", "fgt", "fle", "fge", "const", "id"};
 const std::set<std::string> comm_ops = {"add","mul","eq","and","or","fadd","fmul","feq"};
 const std::string id_prefix = "lvnv_";
 int id_num = 0;
@@ -19,15 +20,23 @@ public:
 
     Value() {}
 
-    Value(json instr){
+    Value(json instr, std::unordered_map<std::string,int>& var_to_id){
         // std::cout << "getting canonical for: " << instr << std::endl;
         canon_instr = instr;
         canon_instr["dest"] = "";
 
         // make canonical representation if commutative
+        auto& args = canon_instr["args"];
         if(comm_ops.count(instr["op"])){
-            auto& args = canon_instr["args"];
             std::sort(args.begin(),args.end());
+        }
+
+        for(auto& arg: args){
+            if(!var_to_id.count(arg)){
+                // std::cout << arg << "missing" << std::endl;
+            }
+            assert(var_to_id.count(arg));
+            arg = "id_" + std::to_string(var_to_id[arg]);
         }
         // std::cout << "got canonical: " << canon_instr << std::endl;
     }
@@ -66,10 +75,11 @@ std::vector<bool> get_last_def(Block& b){
 
 
 void lvn_block(Block& b){
-    std::unordered_map<Value,std::string> table; // value to id
-    std::unordered_map<std::string,std::string> var_to_id; // program var to unique id
-    auto last_def = get_last_def(b);
+    std::unordered_map<Value,int> table_new; // value to id
+    std::unordered_map<std::string,int> var_to_id; // program var
+    std::unordered_map<int,std::string> id_to_var; // id to program var
 
+    auto last_def = get_last_def(b);
     for(int i = 0; i < b.size(); i++){
         auto& instr = b[i];
         // std::cout << "checking instr: " << instr << std::endl;
@@ -78,11 +88,16 @@ void lvn_block(Block& b){
         // std::cout << "updating args" << std::endl;
         if(instr.contains("args")){
             for(auto& var: instr["args"]){
-                // std::cout << "searching for " << var << ", maps to " << var_to_id[var] << std::endl;
+                // std::cout << "searching for " << var << ", contains is " << var_to_id.count(var) << std::endl;
                 if(var_to_id.count(var)){
-                    var = var_to_id[var];
+                    // std::cout << "found var, maps to " << var_to_id[var] << ", which maps to " << id_to_var[var_to_id[var]] << std::endl;
+                    var = id_to_var[var_to_id[var]];
                 } else{
-                    var_to_id[var] = var; // live-in
+                    // live-in
+                    // std::cout << "inserting live in with id " << id_num << ", currently in var " << var << std::endl;
+                    var_to_id[var] = id_num;
+                    id_to_var[id_num] = var;
+                    id_num++;
                 }
             }
         }
@@ -91,42 +106,47 @@ void lvn_block(Block& b){
         Value v;
         bool has_val = instr.contains("op") && known_ops.count(instr["op"]);
         bool new_val = true;
-        std::string id;
-        if(instr.contains("dest")){ id = instr["dest"]; }
+        int dest_id = -1;
 
         if(has_val){
             // std::cout << "is has val op" << std::endl;
-            v = Value(instr);
-            if(table.count(v)){
+            v = Value(instr, var_to_id);
+            if(table_new.count(v)){
                 new_val = false;
                 // std::cout << "found in table" << std::endl;
                 // replace instruction with id if value exists
-                id = table[v];
+                dest_id = table_new[v];
                 auto dest = instr["dest"];
                 // make dead code to be cleaned up
                 b[i] = json{
                     {"dest", dest},
                     {"op", "id"},
                     {"type", instr["type"]},
-                    {"args", {id}}
+                    {"args", {id_to_var[dest_id]}}
                 };
-                var_to_id[dest] = id;
+                var_to_id[dest] = dest_id;
             }
         }
 
         if(instr.contains("dest")){
             // create new id
             std::string dest = instr["dest"];
+            std::string lvnv_name = dest;
             if(!last_def[i]){
-                id = id_prefix + std::to_string(id_num);
+                lvnv_name = id_prefix + std::to_string(id_num);
                 id_num++;
-                instr["dest"] = id;
+                instr["dest"] = lvnv_name;
             }
-            if(has_val && new_val){
-                table[v] = id;
+            if(new_val){
+                dest_id = id_num;
+                id_num++;
+                table_new[v] = dest_id;
+                var_to_id[lvnv_name] = dest_id;
+                id_to_var[dest_id] = lvnv_name;
+                // std::cout << "mapped new value " << v.canon_instr << " to " << dest_id << " in table" << std::endl;
             }
-            var_to_id[dest] = id;
-            // std::cout << "mapped to " << id << " in table" << std::endl;
+            // std::cout << "mapping var " << dest << " to " << dest_id << ", which maps to " << id_to_var[dest_id] << std::endl;
+            var_to_id[dest] = dest_id;
         }
         // std::cout << "updated instr: " << instr << std::endl;
         // std::cout << "iter done" << std::endl << std::endl;
