6ed4a76627fd88e5fe5ed62e835439cac6bba067
Lisa Li <lisaruiwen04@gmail.com>
2025-02-11 13:33:34 -0500

    Write general dataflow framework

--- task4/dataflow.cpp
+++ task4/dataflow.cpp
@@ -9,6 +9,7 @@
 #include "../task2/cfg/cfg_utils.hpp"
 
 
+// get names of variables written to in block [b]
 std::set<std::string> get_defs(const Block& b){
     std::set<std::string> defs;
     for(auto& instr: b){
@@ -19,22 +20,24 @@ std::set<std::string> get_defs(const Block& b){
     return defs;
 }
 
-// performing reaching definitions dataflow analysis
-void df_defined_vars(const json& func){
+// perform defined vars dataflow analysis
+template<typename T, typename M, typename R, typename D>
+void df_worklist(const json& func, bool is_forward, T init, M merge, R transfer, D display){
+    // get CFG
     Cfg cfg = get_cfg_func(func);
     std::vector<Block>& blocks = cfg.blocks;
 
-    // create init
-    std::set<std::string> init;
-    if(func.contains("args")){
-        for(auto arg: func["args"]){
-            init.insert(arg["name"]);
-        }
+    // set direction
+    auto& preds = cfg.preds;
+    auto& succs = cfg.succs;
+    if(!is_forward){
+        succs = cfg.preds;
+        preds = cfg.succs;
     }
 
     // initialize in[entry] and out[*]
-    std::unordered_map<int,std::set<std::string>> in;
-    std::unordered_map<int,std::set<std::string>> out;
+    std::unordered_map<int,T> in;
+    std::unordered_map<int,T> out;
     in[cfg.entryIdx] = init;
     for(int i = 0; i < blocks.size(); i++){
         out[i] = init;
@@ -48,41 +51,69 @@ void df_defined_vars(const json& func){
 
     while(!worklist.empty()){
         int b = worklist.front(); worklist.pop();
-        auto& in_b = in[b];
 
         // merge from predecessors
-        for(int p: cfg.preds[b]){
-            in_b.insert(out[p].begin(), out[p].end());
+        for(int p: preds[b]){
+            merge(in[b], out[p]);
         }
 
         // transfer through block
-        int prev_size = out[b].size();
-        auto defs = get_defs(blocks[b]);
-        defs.insert(in_b.begin(), in_b.end());
-        out[b] = defs;
+        bool changed = transfer(blocks[b], out[b], in[b]);
+        
 
         // queue successors if changed
-        if(defs.size() != prev_size){
-            for(int s: cfg.succs[b]){
+        if(changed){
+            for(int s: succs[b]){
                 worklist.push(s);
             }
         }
     }
+    
+    // display analysis
+    display(blocks.size(), in, out);
+}
 
-    // display defined variables
-    for(int i = 0; i < blocks.size(); i++){
-        std::cout << "\tBlock " << i << std::endl;
-        std::cout << "\t\tin: ";
-        for(auto var: in[i]){
-            std::cout << var << " ";
+void df_defined_vars(const json& func){
+    // create init
+    std::set<std::string> init;
+    if(func.contains("args")){
+        for(auto arg: func["args"]){
+            init.insert(arg["name"]);
         }
-        std::cout << std::endl;
-        std::cout << "\t\tout: ";
-        for(auto var: out[i]){
-            std::cout << var << " ";
-        }
-        std::cout << std::endl;
     }
+
+    // create merge
+    auto merge = [](std::set<std::string>& in_b, const std::set<std::string>& pred){
+        in_b.insert(pred.begin(), pred.end());
+    };
+
+    // create transfer
+    auto transfer = [](const Block& block, std::set<std::string>& out_b, const std::set<std::string>& in_b){
+        int prev_size = out_b.size();
+        auto defs = get_defs(block);
+        defs.insert(in_b.begin(), in_b.end());
+        out_b = std::move(defs);
+        return prev_size != out_b.size();
+    };
+
+    // create display
+    auto display = [](int num_blocks, std::unordered_map<int,std::set<std::string>>& in, std::unordered_map<int,std::set<std::string>>& out){
+        for(int i = 0; i < num_blocks; i++){
+            std::cout << "\tBlock " << i << std::endl;
+            std::cout << "\t\tin: ";
+            for(auto var: in[i]){
+                std::cout << var << " ";
+            }
+            std::cout << std::endl;
+            std::cout << "\t\tout: ";
+            for(auto var: out[i]){
+                std::cout << var << " ";
+            }
+            std::cout << std::endl;
+        }
+    };
+
+    df_worklist(func, true, init, merge, transfer, display);
 }
 
 int main() {
