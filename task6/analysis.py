deba58678f777ab84a4e94b3f5c75c165b5dbc70
Bryant Park <bryantpark04@gmail.com>
2025-03-06 18:58:55 -0500

    Add final analyses

--- task6/analysis.py
+++ task6/analysis.py
@@ -2,11 +2,14 @@ import pandas as pd
 
 
 def main():
-
+    results_dynamic = pd.read_csv("results_dynamic.csv")
     results_static = pd.read_csv("results_static.csv")
 
     for run in ["baseline", "ssa", "roundtrip"]:
-        print(f"{run}: {results_static[results_static.run == run].result.sum()}")
+        print(f"dynamic {run}: {results_dynamic[results_dynamic.run == run].result.sum()}")
+
+    for run in ["baseline", "ssa", "roundtrip"]:
+        print(f"static {run}: {results_static[results_static.run == run].result.sum()}")
 
 
 if __name__ == "__main__":
